9a6ca09877ee140b751b4b8571d0e88b0e5b78bb
yuki <chanyuki1227@yahoo.co.jp>
2023-08-17 14:09:23 +0900

    20230817_PHP4

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+require_once('config/status_codes.php');
+
+$random_numbers = array_rand($status_codes, 4);
+
+foreach ($random_numbers as $index) {
+$options[] = $status_codes[$index];
+}
+
+$question = $options[mt_rand(0, 3)];
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -25,16 +38,19 @@
       <div class="question">
         <p class="question__text">Q. 以下の内容に当てはまるステータスコードを選んでください</p>
         <p class="question__text">
+          <?php echo $question['description'] ?>
         </p>
       </div>
       <form class="quiz-form" action="result.php" method="post">
-        <input type="hidden" name="answer_code" value="">
+        <input type="hidden" name="answer_code" value="<?php echo $question['code'] ?>">
         <div class="quiz-form__item">
+          <?php foreach ($options as $option): ?>
           <div class="quiz-form__group">
-            <input class="quiz-form__radio" id="" type="radio" name="option" value="">
-            <label class="quiz-form__label" for="">
+            <input class="quiz-form__radio" id="<?php echo $option['code'] ?>" type="radio" name="option" value="<?php echo $option['code'] ?>">
+            <label class="quiz-form__label" for="<?php echo $option['code'] ?>">
             </label>
           </div>
+          <?php endforeach; ?>
         </div>
         <div class="quiz-form__button">
           <button class="quiz-form__button-submit" type="submit">
---
+++ result.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status Code Quiz<</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/result.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">
+        Status Code Quiz
+            </a>
+        </div>
+    </header>
+</body>
+    <div class="result__content">
+        <div class="result">
+        <h2 class="result__text--correct">正解</h2>
+        <h2 class="result__text--incorrect">不正解</h2>
+    </div>
+    <div class="answer-table">
+        <table class="answer-table__inner">
+            <tr class="answer-table__row">
+                <th class="answer-table__header">ステータスコード</th>
+                <td class="answer-table__text"></td>
+            </tr>
+            <tr class="answer-table__row">
+                <th class="answer-table__header">説明</th>
+            <td class="answer-table__text"></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
